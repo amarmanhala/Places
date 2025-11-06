b9a16e1898b20e667286d58f60535bb4ed08113a
amarmanhala <amar.manhala@gmail.com>
2025-11-05 22:27:26 -0500

    Initial Commit

---
+++ places/placesApp.swift
@@ -0,0 +1,32 @@
+//
+//  placesApp.swift
+//  places
+//
+//  Created by Amarpreet Singh on 11/5/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct placesApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
